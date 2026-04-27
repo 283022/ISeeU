41b0a057d7da5ca29ad6d1b280663868deca765a
Artem <bafara032@gmail.com>
2026-04-27 22:19:13 +0300

    feat(domain): write Entites: TargetInfo & SurveillanceRule

---
+++ ISeeU.Domain/Entities/SurveillanceRule.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace ISeeU.Domain.Entities;
+
+public class SurveillanceRule(int propertyId, TargetInfo target ,Action<int,object> callback)
+{
+    public int PropertyId { get; } = propertyId;
+    public TargetInfo Target { get; } = target;
+    public Action<int, object> Callback { get; } = callback;
+}
+
---
+++ ISeeU.Domain/Entities/TargetInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace ISeeU.Domain.Entities;
+
+public class TargetInfo(string name)
+{
+    public string Name { get; } = name;
+    public DateTime SubscribeTime { get; } = DateTime.Now;
+    public  List<PropertyChange> Changes { get; set; } = new();
+    
+}
+
+public class PropertyChange
+{
+    public int PropertyId { get; set; }
+    public object NewValue { get; set; }
+    public DateTime Timestamp { get; set; } 
+}
