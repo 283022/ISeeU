36298f50a186f388f6d20b8781b495410f66fea8
Artem <bafara032@gmail.com>
2026-04-30 12:46:41 +0300

    feat(common): add DTO classes for client-server communication
    - Add ElementInfoDto to describe UI element properties
    - Add PropertyInfoDto for supported property metadata

---
+++ ConnectInfo/ElementInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace ConnectInfo;
+
+public class ElementInfo
+{
+    public string ElementId { get; set; }
+    public string Name { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public List<PropertyInfo> Properties { get; set; }
+}
+
+
+
---
+++ ConnectInfo/PropertyInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace ConnectInfo;
+
+public class PropertyInfo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
