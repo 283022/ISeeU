08e417b6a4e44f3116d785b93eb7e8a8968f29ae
Artem <bafara032@gmail.com>
2026-04-30 13:03:25 +0300

    refactor(domain): derive TargetInfo name from IElement instead of separate parameter

--- ISeeU.Domain/Entities/TargetInfo.cs
+++ ISeeU.Domain/Entities/TargetInfo.cs
@@ -2,9 +2,9 @@
 
 namespace ISeeU.Domain.Entities;
 
-public class TargetInfo(string name, IElement element)
+public class TargetInfo(IElement element)
 {
-    public string Name { get; } = name;
+    public string Name { get; } = element.Name;
     public IElement _element { get; } = element;
     public DateTime SubscribeTime { get; } = DateTime.Now;
     public  List<PropertyChange> Changes { get; set; } = new();
