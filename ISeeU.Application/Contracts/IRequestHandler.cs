386cdf4fc195b23e058bdad4cf011728091b0926
Artem <bafara032@gmail.com>
2026-04-30 13:00:28 +0300

    feat(handlers): add ClientRequestHandler for element info requests

---
+++ ISeeU.Application/Contracts/IRequestHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace ISeeU.Application.Contracts;
+
+public interface IRequestHandler<T>
+{
+    bool CanHandle(string message);
+    T Handle(Point  point);
+}
---
+++ ISeeU.Application/RequestHandler/ClientRequestHandler.cs
@@ -0,0 +1,39 @@
+﻿using ISeeU.Application.Contracts;
+using System.Drawing;
+using ConnectInfo;
+
+namespace ISeeU.Application.CommandHandlers;
+
+public class ClientRequestHandler : IRequestHandler<ElementInfo>
+{
+    private readonly IUIAutomationProvider _provider;
+
+    public ClientRequestHandler(IUIAutomationProvider provider)
+    {
+        _provider = provider;
+    }
+
+    public bool CanHandle(string message)
+    {
+        return Equals(message, "request");
+    }
+
+    public ElementInfo Handle(Point coordinates)
+    {
+        // Находим элемент по координатам
+        var element = _provider.FindElement(coordinates);
+
+        // Возвращаем DTO с информацией об элементе
+        return new ElementInfo
+        {
+            Name = element.Name,
+            X = element.BoundingRectangle.X,
+            Y = element.BoundingRectangle.Y,
+            Width = element.BoundingRectangle.Width,
+            Height = element.BoundingRectangle.Height,
+            Properties = element.GetSupportedProperties()
+                .Select(id => new PropertyInfo { Id = id, Name = id.ToString() })
+                .ToList()
+        };
+    }
+}
